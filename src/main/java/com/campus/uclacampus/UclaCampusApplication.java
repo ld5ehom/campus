d6ed8ff1f0a2e5f498623bc0424cfc287df52659
TaeWook Park <ld5ehom@gmail.com>
2024-06-27 01:14:46 -0700

    restart

---
+++ src/main/java/com/campus/uclacampus/UclaCampusApplication.java
@@ -0,0 +1,13 @@
+package com.campus.uclacampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclaCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UclaCampusApplication.class, args);
+	}
+
+}
