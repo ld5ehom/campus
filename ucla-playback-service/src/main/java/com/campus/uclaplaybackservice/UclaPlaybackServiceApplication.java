6b5581911d7fe8ff33f6aa19f4aa3432eaf3f17f
TaeWook Park <ld5ehom@gmail.com>
2024-06-30 16:10:51 -0700

    playback service update

---
+++ ucla-playback-service/src/main/java/com/campus/uclaplaybackservice/UclaPlaybackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.campus.uclaplaybackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclaPlaybackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UclaPlaybackServiceApplication.class, args);
+    }
+
+}
