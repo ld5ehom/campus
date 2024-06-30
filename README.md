19733fecf7fc352d6dfe347cd576ca4446522e0f
TaeWook Park <ld5ehom@gmail.com>
2024-06-29 17:19:20 -0700

    user service test update

---
+++ README.md
@@ -0,0 +1,12 @@
+# UCLA Campus Management System (Backend)
+- ld5ehom@gmail.com
+- https://www.linkedin.com/in/ld5ehom
+
+### Tool
+- Docker : https://www.docker.com/products/docker-desktop/
+- DBeaver : https://dbeaver.io/
+- ERD Editor : https://erd-editor.io/
+- Insomnia : https://insomnia.rest/
+
+
+
---
+++ ucla-file-manage-service/src/main/java/com/campus/uclafilemanageservice/UclaFileManageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.campus.uclafilemanageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclaFileManageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UclaFileManageServiceApplication.class, args);
+	}
+
+}
