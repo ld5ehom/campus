e113804570c548862798cde01df65432e9db632a
TaeWook Park <ld5ehom@gmail.com>
2024-07-02 18:12:21 -0700

    enrollment service init

---
+++ ucla-enrollment-service/src/main/java/com/campus/uclaenrollmentservice/UclaEnrollmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.campus.uclaenrollmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclaEnrollmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UclaEnrollmentServiceApplication.class, args);
+    }
+
+}
---
+++ ucla-enrollment-service/src/test/java/com/campus/uclaenrollmentservice/UclaEnrollmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.uclaenrollmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UclaEnrollmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
