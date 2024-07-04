0ea999f03378775ea32dd14a58d1f5b639efd959
TaeWook Park <ld5ehom@gmail.com>
2024-07-03 17:29:32 -0700

    Enrollment service test

--- ucla-enrollment-service/src/main/java/com/campus/uclaenrollmentservice/UclaEnrollmentServiceApplication.java
+++ ucla-enrollment-service/src/main/java/com/campus/uclaenrollmentservice/UclaEnrollmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.campus.uclaenrollmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UclaEnrollmentServiceApplication {
 
     public static void main(String[] args) {
---
+++ ucla-enrollment-service/src/main/resources/sql/schema-mysql.sql
@@ -0,0 +1,32 @@
+-- Table to store payment information
+CREATE TABLE payments
+(
+    payment_id     INT AUTO_INCREMENT PRIMARY KEY,
+    user_id        INT                                 NOT NULL,
+    payment_type   ENUM ('COURSE', 'SUBSCRIPTION')     NOT NULL, -- Distinguish payment type
+    amount         DECIMAL(10, 2)                      NOT NULL,
+    payment_method VARCHAR(50)                         NOT NULL,
+    payment_date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+-- Table to store course enrollment information
+CREATE TABLE enrollments
+(
+    enrollment_id     INT AUTO_INCREMENT PRIMARY KEY,
+    user_id           INT                                 NOT NULL,
+    course_id         INT                                 NOT NULL,
+    payment_id        INT                                 NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (payment_id) REFERENCES payments (payment_id)
+);
+
+-- Table to store user subscription information
+CREATE TABLE subscriptions
+(
+    subscription_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id         INT                                 NOT NULL,
+    payment_id      INT                                 NOT NULL,
+    start_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    end_date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (payment_id) REFERENCES payments (payment_id)
+);
