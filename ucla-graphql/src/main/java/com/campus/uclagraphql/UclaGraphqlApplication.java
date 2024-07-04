9477eebec1e10b17f2b9759c9323ccf28fb7c572
TaeWook Park <ld5ehom@gmail.com>
2024-07-04 16:47:02 -0700

    graphql init

---
+++ ucla-graphql/src/main/java/com/campus/uclagraphql/UclaGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.campus.uclagraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclaGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UclaGraphqlApplication.class, args);
+    }
+
+}
---
+++ ucla-graphql/src/test/java/com/campus/uclagraphql/UclaGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus.uclagraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UclaGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
